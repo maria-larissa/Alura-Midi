690ef033eed9174c6545e45b010a6be6917d8230
larissa <larissa@debian-BULLSEYE-live-builder-AMD64>
2024-04-23 21:18:04 +0100

    Initial files with frontend design.

---
+++ main.js
@@ -0,0 +1,20 @@
+// usa sempre o nome main para arquivo JS
+
+// document - representa o documento HTML lincado
+
+// pegango um input do HTML document.querySelector('input[type=tel]');
+
+
+function tocarPom(){
+    // pegando tag audio do html para usar no JS
+    document.querySelector('#som_tecla_pom').play();
+    
+}
+
+function tocarClap(){
+    // pegando tag audio do html para usar no JS
+    document.querySelector('#som_tecla_clap').play();
+    
+}
+
+onclick()
