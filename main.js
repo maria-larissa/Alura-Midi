b9f9cde684134ebc4116968b838ed4fd46bdeb52
larissa <larissa@debian-BULLSEYE-live-builder-AMD64>
2024-04-24 20:39:57 +0100

    Keys and sounds working.
    working on style when user use the keyboard to press the keys.

--- main.js
+++ main.js
@@ -1,20 +1,29 @@
-// usa sempre o nome main para arquivo JS
-
-// document - representa o documento HTML lincado
-
-// pegango um input do HTML document.querySelector('input[type=tel]');
-
-
-function tocarPom(){
-    // pegando tag audio do html para usar no JS
-    document.querySelector('#som_tecla_pom').play();
-    
+// Funtion to play the respective sound of the key
+function play(idElementAudio){
+   document.querySelector(idElementAudio).play();
 }
 
-function tocarClap(){
-    // pegando tag audio do html para usar no JS
-    document.querySelector('#som_tecla_clap').play();
-    
-}
+// List of keys on Alura MI
+const keysList = document.querySelectorAll('.tecla');
 
-onclick()
+for(let cont =0 ; cont < keysList.length ; cont++ ){
+    
+    const key = keysList[cont];
+    const classOfKey = key.classList[1];
+
+    // Creating the audio I dinamic.
+    const idAudio = `#som_${classOfKey}`;
+
+    // function play audio when key is clicked
+    key.onclick = function(){
+        play(idAudio);
+    }
+    
+    key.onkeydown = function(){
+        key.classList.add('ativa');
+    }
+    
+    key.onkeyup = function(){
+        key.classList.remove('ativa');      
+    }
+}
